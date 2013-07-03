1a75482630ebd69ad8b6a2d6b5d9117a8b0933dd
Zach Donnell <slick50zd1@gmail.com>
2013-07-03 15:58:10 -0700

    Refactored Car and Wheel for better reproduction and mutation

--- src/com/zdonnell/geneticcars/Car.java
+++ src/com/zdonnell/geneticcars/Car.java
@@ -1,86 +1,169 @@
 package com.zdonnell.geneticcars;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  *
  * @author Zach
  */
 public class Car {
 
+    private final static int  ATTRIBUTE_COUNT = 14;
+
+    private final static float CHASSIS_MAX_AXIS = 1.1f;
+    private final static float CHASSIS_MIN_AXIS = 0.1f;
+
     /**
-     * The genetic representation of the Car's phenotypes.
+     * Represents the raw values for the chassis bounds.
      */
-    private boolean[] dna;
+    private Vector2[] bodySegments;
 
     /**
-     * The number of segments in the car's body.
+     * Represents the raw values for the wheel definitions.
      */
-	private final static int BODY_SEGMENTS = 8;
+    private Wheel[] wheels;
 
     /**
-     * The max length of the car's body segments
+     * Creates a car with random attributes.
      */
-	private final static int MAX_BODY_SEGMENT_LEN = 10;
+    public Car() {
+        generateRandomAttributes();
+    }
 
     /**
-     * The max number of wheels a car can have
-     */
-    private final static int MAX_WHEELS = 2;
-
-    /**
-     * An array the size of {@link #BODY_SEGMENTS} that stores
-     * the length of each for this car.
-     */
-	private double[] bodySegmentLengths = new double[BODY_SEGMENTS];
-
-    /**
-     * An array the size of {@link #BODY_SEGMENTS} that stores
-     * the angle of each segment. This number represents the offset from the
-     * previous angle, not the total angle.
-     */
-	private double[] bodySegmentAngles = new double[BODY_SEGMENTS];
-
-    /**
-     * The actual number of wheels for this car.
-     */
-    private int wheelCount;
-
-	private Wheel[] wheels;
-
-    private int[] wheelMountPoints;
-
-    /**
-     * Generates a car based on the provided dna, or randomly generated one if
-     * a null dna is provide.
+     * Creates a new car based on the provided raw data
      *
-     * @param dna the dna to express for this car, can be null
+     * @param bodySegments
+     * @param wheels
      */
-	public Car(boolean[] dna) {
-        if (dna == null) {
-            dna = randomGenome();
+    public Car(Vector2[] bodySegments, Wheel[] wheels) {
+        this.bodySegments = bodySegments;
+        this.wheels = wheels;
+    }
+
+    /**
+     * Generates random attributes for the car's chassis
+     * and it's wheels.
+     */
+    private void generateRandomAttributes() {
+        // Create 2 random wheels
+        wheels = new Wheel[2];
+        for (int i = 0; i < 2; i++) {
+            wheels[i] = new Wheel();
         }
-        this.dna = dna;
-        expressGenome(dna);
-	}
+
+        // Create 8 random body segments
+        bodySegments = new Vector2[8];
+        for (int j = 0; j < 8; j++) {
+            float randx = (float) Math.random() * CHASSIS_MAX_AXIS + CHASSIS_MIN_AXIS;
+            float randy = (float) Math.random() * CHASSIS_MAX_AXIS + CHASSIS_MIN_AXIS;
+            bodySegments[j] = new Vector2(randx, randy);
+        }
+    }
 
     /**
-     * Sets the cars attributes based on the specified dna
-     *
-     * @param dna
+     * Returns the raw values for the wheel definitions.
      */
-	private void expressGenome(boolean[] dna) {
-
+    private Wheel[] getWheels() {
+        return wheels;
     }
 
-    private boolean[] randomGenome() {
-        return null;
+    /**
+     * Returns the raw values for the chassis bounds.
+     */
+    private Vector2[] getBodySegments() {
+        return bodySegments;
     }
 
-    public boolean[] getDna() {
-        return dna;
+    public Car mateWith(Car mate, float mutateFactor) {
+        Car child = geneticCrossover(this, mate);
+        child.mutate(mutateFactor);
+
+        return child;
     }
 
-    public Car mateWith(Car mate) {
-        boolean[] childDna = GeneticAlgorithms.reproduce(this.getDna(), mate.getDna());
-        return new Car(childDna);
+    /**
+     * Mimics genetic chromosome crossover found in
+     * sexual reproduction.<br><br>
+     *
+     * This method picks a point in the figurative list of
+     * attributes to alternate which parent attributes it uses for the
+     * child.<br><br>
+     *
+     * For example, if the crossover point is 8 all attributes before 8 will
+     * be pulled from parent one, the remainder will be pulled from parent two.<br><br>
+     *
+     * For our case the attributes look like this
+     * <ol>
+     *     <li>Body Segment 1 (Vector2)</li>
+     *     <li>Body Segment 2 (Vector2)</li>
+     *     <li>Body Segment 3 (Vector2)</li>
+     *     <li>Body Segment 4 (Vector2)</li>
+     *     <li>Body Segment 5 (Vector2)</li>
+     *     <li>Body Segment 6 (Vector2)</li>
+     *     <li>Body Segment 7 (Vector2)</li>
+     *     <li>Body Segment 8 (Vector2)</li>
+     *     <li>Wheel 1 Density</li>
+     *     <li>Wheel 1 Radius</li>
+     *     <li>Wheel 1 Vertex</li>
+     *     <li>Wheel 2 Density</li>
+     *     <li>Wheel 2 Radius</li>
+     *     <li>Wheel 2 Vertex</li>
+     * </ol>
+     *
+     * @param p1 the first parent used in reproduction
+     * @param p2 the second parent used in reproduction
+     * @return the new born child
+     */
+    public static Car geneticCrossover(Car p1, Car p2) {
+        int split = (int) Math.random() * ATTRIBUTE_COUNT;
+
+        // Select body segments from parents
+        Vector2[] childBodySegs = new Vector2[8];
+        int segNum = 0;
+        for (;segNum < 8; segNum++) {
+            childBodySegs[segNum] = (segNum < split) ? p1.getBodySegments()[segNum].cpy()
+                                                     : p2.getBodySegments()[segNum].cpy();
+        }
+
+        Wheel[] wheels = new Wheel[2];
+        // Base wheel 1 from parents
+        double w1Den = (segNum + 1 < split) ? p1.wheels[0].getDensity() : p2.wheels[0].getDensity();
+        double w1Rad = (segNum + 2 < split) ? p1.wheels[0].getRadius() : p2.wheels[0].getRadius();
+        int w1Vert = (segNum + 3 < split) ? p1.wheels[0].getVertex() : p2.wheels[0].getVertex();
+        wheels[0] = new Wheel(w1Den, w1Rad, w1Vert);
+
+        // Base wheel 2 from parents
+        double w2Den = (segNum + 1 < split) ? p1.wheels[1].getDensity() : p2.wheels[1].getDensity();
+        double w2Rad = (segNum + 2 < split) ? p1.wheels[1].getRadius() : p2.wheels[1].getRadius();
+        int w2Vert = (segNum + 3 < split) ? p1.wheels[1].getVertex() : p2.wheels[1].getVertex();
+        wheels[1] = new Wheel(w2Den, w2Rad, w2Vert);
+
+        return new Car(childBodySegs, wheels);
+    }
+
+    /**
+     * This applies mutations to each attribute (for the sake of this
+     * program, a mutation is a new random value for that attribute) with a probability
+     * of that provided.<br><br>
+     *
+     * For example, providing a mutation factor of 0.05 (5%) would give each
+     * attribute a 5% chance of being randomized.
+     *
+     * @param mutateFactor the mutation factor
+     */
+    private void mutate(float mutateFactor) {
+        // check for body/chassis mutations
+        for (int j = 0; j < 8; j++) {
+            if (Math.random() < mutateFactor) {
+                float randx = (float) Math.random() * CHASSIS_MAX_AXIS + CHASSIS_MIN_AXIS;
+                float randy = (float) Math.random() * CHASSIS_MAX_AXIS + CHASSIS_MIN_AXIS;
+                bodySegments[j] = new Vector2(randx, randy);
+            }
+        }
+        // check for wheel mutations
+        for (int i = 0; i < 2; i++) {
+            wheels[i].mutate(mutateFactor);
+        }
     }
 }
--- src/com/zdonnell/geneticcars/Wheel.java
+++ src/com/zdonnell/geneticcars/Wheel.java
@@ -1,22 +1,91 @@
 package com.zdonnell.geneticcars;
 
 public class Wheel {
-	private final static int WHEEL_MAX_MASS = 100;
-	private final static int WHEEL_MAX_RADIUS = 5;
 
-	private final double mass;
-	private final double radius;
+    private final static double WHEEL_MAX_RADIUS = 0.5;
+    private final static double WHEEL_MIN_RADIUS = 0.2;
+    private final static int WHEEL_MAX_DENSITY = 100;
+    private final static int  WHEEL_MIN_DENSITY = 40;
 
+    /**
+     * How dense the wheel is (more dense means more weight for the same radius)
+     *
+     * @see {@link #WHEEL_MAX_DENSITY}
+     * @see {@link #WHEEL_MIN_DENSITY}
+     */
+	private double density;
+
+    /**
+     * Radius of the wheel
+     *
+     * @see {@link #WHEEL_MAX_RADIUS}
+     * @see {@link #WHEEL_MIN_RADIUS}
+     */
+	private double radius;
+
+    /**
+     * The mount point for the wheel on the car chassis, as a vertex number
+     */
+    private int vertex;
+
+    /**
+     * Generates a random wheel
+     */
 	public Wheel() {
-		mass = Math.random() * WHEEL_MAX_MASS;
-		radius = Math.random() * WHEEL_MAX_RADIUS;
+        density = Math.random() * WHEEL_MAX_DENSITY + WHEEL_MIN_DENSITY;
+		radius = Math.random() * WHEEL_MAX_RADIUS + WHEEL_MIN_RADIUS;
+        vertex = (int) Math.floor(Math.random() * 8) % 8;
 	}
 
-	public double getMass() {
-		return mass;
+    /**
+     * Generates a wheel with the specified attributes
+     *
+     * @param density
+     * @param radius
+     * @param vertex
+     */
+    public Wheel(double density, double radius, int vertex) {
+        this.density = density;
+        this.radius = radius;
+        this.vertex = vertex;
+    }
+
+    /**
+     * This method is available so during reproduction, the parent car
+     * can have it's wheel's mutate
+     *
+     * @see {@link Car#mutate(float)}
+     */
+    public void mutate(float mutateFactor) {
+        if (Math.random() < mutateFactor)
+            density = Math.random() * WHEEL_MAX_DENSITY + WHEEL_MIN_DENSITY;
+        if (Math.random() < mutateFactor)
+            radius = Math.random() * WHEEL_MAX_RADIUS + WHEEL_MIN_RADIUS;
+        if (Math.random() < mutateFactor)
+            vertex = (int) Math.floor(Math.random() * 8) % 8;
+    }
+
+    /**
+     *
+     * @return the wheel's density attribute.
+     */
+	public double getDensity() {
+		return density;
 	}
 
+    /**
+     *
+     * @return the wheel's radius attribute.
+     */
 	public double getRadius() {
 		return radius;
 	}
+
+    /**
+     *
+     * @return the wheel's mount vertex
+     */
+    public int getVertex() {
+        return vertex;
+    }
 }
