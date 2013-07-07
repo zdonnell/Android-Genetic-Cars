81c08cc7124049b4c17b8236896eb16681ec847d
Zach Donnell <slick50zd1@gmail.com>
2013-07-07 13:20:33 -0700

    added a few more comments to Wheel.java

--- src/com/zdonnell/geneticcars/Wheel.java
+++ src/com/zdonnell/geneticcars/Wheel.java
@@ -1,10 +1,30 @@
 package com.zdonnell.geneticcars;
 
+/**
+ * Similar to {@link CarDefinition} this class is primarily a blueprint
+ * for the {@link CarFactory} to build box2d physics objects for the wheels
+ * from.
+ */
 public class Wheel {
 
+	/**
+	 * The largest a wheel can be
+	 */
 	public final static double WHEEL_MAX_RADIUS = 0.5;
+
+	/**
+	 *  The smallest a wheel can be
+	 */
 	public final static double WHEEL_MIN_RADIUS = 0.2;
+
+	/**
+	 * The maximum density (in box2d) for a wheel
+	 */
 	public final static int WHEEL_MAX_DENSITY = 100;
+
+	/**
+	 * The minimum density (in box2d) for a wheel
+	 */
 	public final static int WHEEL_MIN_DENSITY = 40;
 
 	/**
